db6987ef49bcb617100eb08f2c98bed560aefd10
fortunetede <fortune2test@gmail.com>
2018-09-06 07:19:55 +0000

    fix create user

---
+++ trendz/views.py
@@ -0,0 +1,71 @@
+
+
+from django.shortcuts import render, redirect
+from trendz.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+from trendz.forms import EditProfileForm
+
+
+def home(request):
+	return render(request, "home/homepage.html") 
+
+
+def news(request):
+	return render(request, "home/news.html") 
+
+def register(request):
+	if request.method == 'POST':
+		print 'came in'
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			print 'form is valid'
+			new_user = form.save(commit=False)
+			new_user.save() 
+			print 'for msaved'
+			return render(request, 'home/news.html')
+	print 'form not valid'
+	args = {'form':form}	
+	return render(request, 'firstPage/register.html', args)
+
+
+def view_profile(request):
+	args = {'user':request.user}
+	return render(request, "accounts/profile.html", args)
+
+
+def edit_profile(request):
+	if request.method == 'POST':
+		form = EditProfileForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+
+			return redirect("/trendz/profile/")
+	else:
+		form = EditProfileForm(instance=request.user)
+
+		args = { 'form':form }
+		return render(request, 'accounts/edit_profile.html', args)
+
+
+def change_password(request):
+	if request.method == 'POST':
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+
+		if form.is_valid():
+			form.save()
+			update_session_auth_hash(request, form.user)
+
+			return redirect("/trendz/profile/")
+
+	else:
+		form = PasswordChangeForm(user=request.user)
+		args = {'form': form}
+
+		return render(request, 'accounts/edit_profile.html', args)
+
+
+#B1tf11nex123
